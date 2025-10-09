322f3ac27a9d24c1125edf60eb3afb33c4a1b9f5
Filip Højgaard <filip.hojgaard@gmail.com>
2025-10-09 22:22:27 +0200

    Added UI for scoring. It is currently not connected to the scoring in GameManager.

---
+++ Src/LeftScoreLabel.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+namespace Prong.Src;
+
+public partial class LeftScoreLabel : Node
+{
+    public override void _Process(double delta)
+    {
+        // Get labels references
+        // Set labels text value to GameManger.Player1Score and player2score. 
+    }
+
+}
