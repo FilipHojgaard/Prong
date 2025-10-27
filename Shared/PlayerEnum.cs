5098558734e31c98fd68d62ba0ebcefc6d9c509c
Filip Højgaard <filip.hojgaard@gmail.com>
2025-10-27 22:03:07 +0100

    Added shared folder with PlayerEnum and ProngConstants

---
+++ Shared/PlayerEnum.cs
@@ -0,0 +1,8 @@
+namespace Prong.Shared;
+
+public enum PlayerEnum
+{
+    Undefined = 0,
+    LeftPlayer = 1,
+    RightPlayer = 2,
+}
---
+++ Shared/ProngConstants.cs
@@ -0,0 +1,6 @@
+namespace Prong.Shared;
+
+public static class ProngConstants
+{
+    public const string EventHubPath = "/root/Eventbus";
+}
