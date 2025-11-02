27c40f174699d317c90ad0453fcf78dc3658d876
Filip Højgaard <filip.hojgaard@gmail.com>
2025-11-02 21:52:19 +0100

    Fireball entity now initializer to define speed and a DiagonalTypeEnum for controlling the direction.

---
+++ Shared/DiagonalTypeEnum.cs
@@ -0,0 +1,10 @@
+namespace Prong.Shared;
+
+
+public enum DiagonalTypeEnum
+{
+    Undefined = 0,
+    Straight = 1,
+    Upwards = 2,
+    Downwards = 3,
+};
--- Src/Fireball.cs
+++ Src/Fireball.cs
@@ -5,8 +5,11 @@ namespace Prong.Src.Blocks;
 
 public partial class Fireball : RigidBody2D
 {
-    public int Speed { get; set; } = 900;
+    public int Speed { get; set; }
     public bool HitBall { get; set; } = false;
+    public DiagonalTypeEnum Diagonal { get; set; }
+
+    // TODO: Implement diagonal here, and an Initialize method to set it. then in LinearVelocity under _Process, we can set the 0 to be the diaganal value or 0 dynamically for level 3 attack. 
 
     public override void _Ready()
     {
@@ -22,8 +25,30 @@ public partial class Fireball : RigidBody2D
 
     public override void _Process(double delta)
     {
-        LinearVelocity = new Vector2(Speed, 0);
+        float yDirection;
+        switch (Diagonal)
+        {
+            case DiagonalTypeEnum.Straight:
+                yDirection = 0f;
+                break;
+            case DiagonalTypeEnum.Upwards:
+                yDirection = -0.08f;
+                break;
+            case DiagonalTypeEnum.Downwards:
+                yDirection = 0.08f;
+                break;
+            default:
+                yDirection = 0f;
+                break;
+        }
+        LinearVelocity = new Vector2(Speed, 0).Rotated(yDirection);
         CheckForLeftMap();
+    }
+
+    public void Initialize(int speed, DiagonalTypeEnum diagonal = DiagonalTypeEnum.Straight)
+    {
+        Speed = speed;
+        Diagonal = diagonal;
     }
 
     public void FireLeft()
